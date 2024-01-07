bbcd6567ad1b21535b5124e2bc262e203ab2af40
松村勝広 <matsumurakatsuhiro@matsumurakatsuhironoMacBook-Air.local>
2024-01-07 21:14:58 +0900

    戦闘の仕組みの実装

---
+++ Assets/PlayMode/Usecase/UI/Charcter/EnemyStatusView.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStatusView : MonoBehaviour
+{
+    public List<Text> CharacterStatusText;
+
+    public void SetPlayerStatus(List<Character> playerList)
+    {
+        for (int i = 0; i < CharacterStatusText.Count; i++)
+        {
+            if(playerList.Count > i){
+            CharacterStatusText[i].text = playerList[i].Name + "\n" + "HP:" + playerList[i].Hp + "\n" + "MP:" + playerList[i].Mp;
+            }
+            else
+            {
+                CharacterStatusText[i].text = "";
+            }
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
